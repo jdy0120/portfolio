ae2250d87f833a7fb22a8dab484c490844e26b14
mango <jdy0120120@gmail.com>
2022-12-03 10:16:11 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   env: {
-    BASE_URL: process.env.TISTORY_ACCESS_TOKEN,
+    NEXT_PUBLIC_TISTORY_ACCESSTOKEN:
+      process.env.NEXT_PUBLIC_TISTORY_ACCESSTOKEN,
+    NEXT_PUBLIC_YOUTUBE_ACCESSTOKEN:
+      process.env.NEXT_PUBLIC_YOUTUBE_ACCESSTOKEN,
   },
   images: {
     domains: ["i.ytimg.com"],

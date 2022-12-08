d580b23c46d16cd48291a4b04522f4d4b154eec3
mango <jdy0120120@gmail.com>
2022-12-08 13:12:38 +0900

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "http://portfolio.doyeonism.com/",
+  siteUrl: "https://portfolio.doyeonism.com/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };

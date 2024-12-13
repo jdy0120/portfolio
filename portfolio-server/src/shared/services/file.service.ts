7140ce0a3c9f1dfeda17ef23b7d530944b1bbf1d
Doyeonism <jdy0120120@gmail.com>
2024-12-13 09:34:29 +0900

    update: file upload s3

--- portfolio-server/src/shared/services/file.service.ts
+++ portfolio-server/src/shared/services/file.service.ts
@@ -1,11 +1,18 @@
-import { Request, Response } from "express";
+import { Request } from "express";
 
 import { parseFormData } from "../utils/file";
 import { seq } from "../../shared/configs/sequelize.config";
+import { v4 as uuidv4 } from "uuid";
+import s3 from "../../shared/configs/aws.config";
+import fs from "fs/promises";
 import {
   AttachmentTemp,
   AttachmentTempCreationAttributes,
 } from "../models/v1/attachment.model";
+import {
+  PutObjectCommand,
+  PutObjectCommandInput,
+} from "@aws-sdk/client-s3";
 
 const upload = async (req: Request) => {
   const { files } = await parseFormData(req);
@@ -16,12 +23,26 @@ const upload = async (req: Request) => {
 
     for (const file of files["files"]) {
       const filename = file.newFilename;
-      const path = `./uploads/${filename}`;
+      const extension = file.mimetype?.split("/")[1];
+
+      const uniqueFilename = `${uuidv4()}-${filename}.${extension}`;
+
+      const buffer = await fs.readFile(file.filepath);
+
+      const s3Params: PutObjectCommandInput = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME || "",
+        Key: `assets/images/${uniqueFilename}`,
+        Body: buffer,
+        ContentType: file.mimetype as string,
+      };
+
+      const command = new PutObjectCommand(s3Params);
+      await s3.send(command);
 
       attachmentTempList.push({
         filename,
         originalFilename: file.originalFilename as string,
-        path,
+        path: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/assets/images/${uniqueFilename}`,
         mimetype: file.mimetype as string,
         size: file.size,
       });

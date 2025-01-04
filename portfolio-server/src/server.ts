67502b25da65c6651e123a41f3e83430bd635bcf
Doyeonism <jdy0120120@gmail.com>
2025-01-05 00:16:38 +0900

    update: update azure ssh key

--- portfolio-server/src/server.ts
+++ portfolio-server/src/server.ts
@@ -7,8 +7,6 @@ const runServer = async () => {
   const app = (await import("./shared/configs/express.config"))
     .default;
 
-  console.log("update azure ssh key");
-
   // Start Server
   const PORT = process.env.PORT;
   app.listen(PORT, async () => {

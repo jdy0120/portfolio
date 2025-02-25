58b43f041b6dade5762c5b3c296a1aa2533dec50
Doyeonism <jdy0120120@gmail.com>
2025-02-25 10:00:03 +0900

    feat: update admin providers

--- admin/src/app/(route)/layout.tsx
+++ admin/src/app/(route)/layout.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import LoginCheck from "../../lib/providers/LoginCheck";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const layout = ({ children }: Props) => {
-  const queryClient = new QueryClient();
-
   return <LoginCheck>{children}</LoginCheck>;
 };
 

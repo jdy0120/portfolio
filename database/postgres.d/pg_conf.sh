bb1733e9fbda803d924d85620fc376fb095c203f
Doyeonism <jdy0120120@gmail.com>
2025-03-04 18:21:33 +0900

    feat: update docker compose

--- database/postgres.d/pg_conf.sh
+++ database/postgres.d/pg_conf.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-# PostgreSQL 설정 파일 경로
-PG_CONF="$PGDATA/postgresql.conf"
-HBA_CONF="$PGDATA/pg_hba.conf"
+# # PostgreSQL 설정 파일 경로
+# PG_CONF="$PGDATA/postgresql.conf"
+# HBA_CONF="$PGDATA/pg_hba.conf"
 
-# 외부 접속 허용 설정
-echo "listen_addresses = '*'" >> "$PG_CONF"
-echo "host all all 0.0.0.0/0 md5" >> "$HBA_CONF"
+# # 외부 접속 허용 설정
+# echo "listen_addresses = '*'" >> "$PG_CONF"
+# echo "host all all 0.0.0.0/0 md5" >> "$HBA_CONF"
